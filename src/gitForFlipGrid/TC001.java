ba5f82c946ebf3b12fecc3ccefaa2471e3b8d10b
nevin <aydingezernevin@gmail.com>
2022-07-01 12:41:28 +0300

    Futher commit

--- src/gitForFlipGrid/TC001.java
+++ src/gitForFlipGrid/TC001.java
@@ -1,5 +1,6 @@
 package gitForFlipGrid;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TC001 {
@@ -10,6 +11,13 @@ public class TC001 {
         gitCommands[2]="gitMerge";
         gitCommands[3]="gitPull";
         gitCommands[4]="gitCommitAndPush";
+        System.out.println(Arrays.toString(gitCommands));
+        System.out.println("gitCommands = " + Arrays.toString(gitCommands));
+        ArrayList<String> gitCommandsList = new ArrayList<>(Arrays.asList(gitCommands));
+        System.out.println(gitCommandsList);
+        gitCommandsList.add("gitCheckout");
+        gitCommandsList.add("gitPush");
+        System.out.println("gitCommandsList = " + gitCommandsList);
 
 
     }
