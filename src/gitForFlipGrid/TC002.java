44855af159c9f26773ec59c34f48ed27b62ca75c
nevin <aydingezernevin@gmail.com>
2022-07-01 12:51:31 +0300

    Further commit

--- src/gitForFlipGrid/TC002.java
+++ src/gitForFlipGrid/TC002.java
@@ -3,5 +3,7 @@ package gitForFlipGrid;
 public class TC002 {
     public static void main(String[] args) {
         System.out.println("akshf;akhfg;hafg;a");
+        System.out.println("jzfkjzlfkhzl");
+        System.out.println("k");
     }
 }
