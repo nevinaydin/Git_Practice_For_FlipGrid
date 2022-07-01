242344a5860026946d5fb63e625cb3ef36a70828
nevin <aydingezernevin@gmail.com>
2022-07-01 12:31:56 +0300

    Futher commit

---
+++ src/gitForFlipGrid/TC001.java
@@ -0,0 +1,16 @@
+package gitForFlipGrid;
+
+import java.util.Arrays;
+
+public class TC001 {
+    public static void main(String[] args) {
+        String[] gitCommands=new String[5];
+        gitCommands[0]= "gitCommit";
+        gitCommands[1]="gitFetch";
+        gitCommands[2]="gitMerge";
+        gitCommands[3]="gitPull";
+        gitCommands[4]="gitCommitAndPush";
+
+
+    }
+}
